611d8ca879526c408d5f7509258975cb90d0cb9d
Duc Nguye <duc.nguyen@codestringers.com>
2018-05-24 22:34:04 +0700

    adding icon email and password in login

--- app/src/main/java/com/medicnet/android/authentication/selectorganization/OrganizationFragment.java
+++ app/src/main/java/com/medicnet/android/authentication/selectorganization/OrganizationFragment.java
@@ -4,17 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.medicnet.android.R;
 
-import dagger.android.support.AndroidSupportInjection;
-
 public class OrganizationFragment extends Fragment {
 
     private static final String JSON_DATA = "json_data";
+    private final String TAG = getClass().getSimpleName();
 
     public static OrganizationFragment newInstance(String json) {
 
@@ -28,13 +28,14 @@ public class OrganizationFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AndroidSupportInjection.inject(this);
+//        AndroidSupportInjection.inject(this);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_select_organization, container, false);
+        Log.d(TAG, "json data>> " + getArguments().getString(JSON_DATA));
         return view;
     }
 }
--- app/src/main/java/com/medicnet/android/authentication/signup/ui/SignupFragment.kt
+++ app/src/main/java/com/medicnet/android/authentication/signup/ui/SignupFragment.kt
@@ -12,6 +12,7 @@ import android.view.ViewTreeObserver
 import com.medicnet.android.R
 import com.medicnet.android.authentication.signup.presentation.SignupPresenter
 import com.medicnet.android.authentication.signup.presentation.SignupView
+import com.medicnet.android.authentication.ui.AuthenticationActivity
 import com.medicnet.android.helper.KeyboardHelper
 import com.medicnet.android.helper.TextHelper
 import com.medicnet.android.util.extensions.*
@@ -54,10 +55,10 @@ class SignupFragment : Fragment(), SignupView {
         relative_layout.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
 
         setUpNewUserAgreementListener()
-
-        /*txvOrganization.setOnClickListener {
-                        presenter.toSelectOrganization(((AuthenticationActivity)activity).organizationJson)
-        }*/
+        txvOrganization.setOnClickListener {
+            var context: AuthenticationActivity = activity as AuthenticationActivity
+            presenter.toSelectOrganization(context.organizationJson)
+        }
         button_sign_up.setOnClickListener {
             presenter.signup(text_name.textContent, text_username.textContent, txvRole.textContent, txvOrganization.textContent, text_password.textContent, text_email.textContent)
         }
