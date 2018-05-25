966a5249ccf53a3e5fd915694e8c07486479d0b9
duc.nguyen <duc.nguyen@filestring.com>
2018-05-25 16:30:04 +0700

    Adding role fragment

---
+++ app/src/main/java/com/medicnet/android/authentication/selectrole/RoleFragment.java
@@ -0,0 +1,14 @@
+package com.medicnet.android.authentication.selectrole;
+
+import com.medicnet.android.authentication.selectorganization.OrganizationFragment;
+
+/**
+ * @author duc.nguyen
+ * @since 5/25/2018
+ */
+public class RoleFragment extends OrganizationFragment {
+    @Override
+    protected void setupListViewAdapter() {
+        super.setupListViewAdapter();
+    }
+}
