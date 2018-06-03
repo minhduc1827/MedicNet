7295cf103347870a4b473010797d7b1d0610d6de
Duc Nguyen <minhduc1827@gmail.com>
2018-06-03 09:30:40 +0700

    Moving myvault to chatroomsfragment

--- app/src/main/java/com/medicnet/android/chatrooms/ui/ChatRoomsFragment.kt
+++ app/src/main/java/com/medicnet/android/chatrooms/ui/ChatRoomsFragment.kt
@@ -1,6 +1,8 @@
 package com.medicnet.android.chatrooms.ui
 
+import DateTimeHelper
 import android.app.AlertDialog
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.app.Fragment
@@ -10,9 +12,12 @@ import android.support.v7.util.DiffUtil
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.SearchView
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.*
 import android.widget.CheckBox
 import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.core.view.isVisible
 import chat.rocket.common.model.RoomType
 import chat.rocket.core.internal.realtime.socket.model.State
@@ -33,7 +38,8 @@ import com.medicnet.android.widget.DividerItemDecoration
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_chat_rooms.*
-import kotlinx.android.synthetic.main.nav_medicnet_header.*
+import kotlinx.android.synthetic.main.item_my_vault.*
+import kotlinx.android.synthetic.main.unread_messages_badge_my_vault.*
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.NonCancellable.isActive
 import timber.log.Timber
@@ -55,6 +61,8 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
     private var sectionedAdapter: SimpleSectionedRecyclerViewAdapter? = null
     val TAG: String = ChatRoomsFragment::class.java.simpleName
     var itemRecyclerView: View? = null
+    var isMyVaultClicked: Boolean = false
+    var mainActivity: MainActivity? = null
 
     companion object {
         val TAG: String = "ChatRoomsFragment"
@@ -85,11 +93,111 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
         setupToolbar()
         setupRecyclerView()
         presenter.loadChatRooms()
+        mainActivity = activity as MainActivity
+        layoutMyVault.setOnClickListener {
+            LogUtil.d(TAG, "onClick my vault")
+            var tagChatRoom: ChatRoom? = null
+            if (layoutMyVault.tag != null)
+                tagChatRoom = layoutMyVault.tag as ChatRoom
+            if (tagChatRoom != null) {
+                mainActivity!!.drawer_layout.closeDrawer(Gravity.START)
+//                val fragment = supportFragmentManager.findFragmentByTag(ChatRoomsFragment.TAG) as ChatRoomsFragment
+                changeItemBgColor(Color.WHITE)
+                loadChatRoom(tagChatRoom)
+            }
+        }
     }
 
     override fun onDestroyView() {
         listJob?.cancel()
         super.onDestroyView()
+    }
+
+    fun setupMyVault(chatRoom: ChatRoom?) {
+        if (chatRoom != null) {
+            text_last_message_my_vault.setVisible(true)
+            text_last_message_date_time_my_vault.setVisible(true)
+            bindLastMessageDateTime(chatRoom, text_last_message_date_time_my_vault)
+            bindLastMessage(chatRoom, text_last_message_my_vault)
+            bindUnreadMessages(chatRoom, text_total_unread_messages_my_vault)
+            if (chatRoom.alert || chatRoom.unread > 0) {
+                text_chat_name_my_vault.setTextColor(ContextCompat.getColor(activity!!,
+                        R.color.red_dark))
+                text_last_message_date_time_my_vault.setTextColor(ContextCompat.getColor(activity!!,
+                        R.color.colorAccent))
+                text_last_message_my_vault.setTextColor(ContextCompat.getColor(activity!!,
+                        android.R.color.primary_text_light))
+            } else {
+                text_chat_name_my_vault.setTextColor(ContextCompat.getColor(activity!!,
+                        R.color.red_dark))
+                text_last_message_date_time_my_vault.setTextColor(ContextCompat.getColor(activity!!,
+                        R.color.colorSecondaryText))
+                text_last_message_my_vault.setTextColor(ContextCompat.getColor(activity!!,
+                        R.color.colorSecondaryText))
+            }
+            /*if (!isMyVaultClicked) {
+                layoutMyVault.performClick()
+                isMyVaultClicked = true
+            }*/
+        }
+    }
+
+    private fun bindLastMessageDateTime(chatRoom: ChatRoom, textView: TextView) {
+        val lastMessage = chatRoom.lastMessage
+        if (lastMessage != null) {
+            val localDateTime = DateTimeHelper.getLocalDateTime(lastMessage.timestamp)
+            textView.content = DateTimeHelper.getDate(localDateTime, activity!!)
+        } else {
+            textView.content = ""
+        }
+    }
+
+    private fun bindLastMessage(chatRoom: ChatRoom, textView: TextView) {
+        val lastMessage = chatRoom.lastMessage
+        val lastMessageSender = lastMessage?.sender
+        if (lastMessage != null && lastMessageSender != null) {
+            val message = lastMessage.message
+            val senderUsername = lastMessageSender.name ?: lastMessageSender.username
+            when (senderUsername) {
+                chatRoom.name -> {
+                    textView.content = message
+                }
+                else -> {
+                    /*val user = if (localRepository.checkIfMyself(lastMessageSender.username!!)) {
+                        "${this.getString(R.string.msg_you)}: "
+                    } else {
+                        "$senderUsername: "
+                    }*/
+                    val user = if (mainActivity!!.username.equals(chatRoom.name)) {
+                        "${this.getString(R.string.msg_you)}: "
+                    } else {
+                        "$senderUsername: "
+                    }
+                    val spannable = SpannableStringBuilder(user)
+                    val len = spannable.length
+                    spannable.setSpan(ForegroundColorSpan(Color.BLACK), 0, len - 1, 0)
+                    spannable.append(message)
+                    textView.content = spannable
+                }
+            }
+        } else {
+//            textView.content = getText(R.string.msg_no_messages_yet)
+        }
+    }
+
+    private fun bindUnreadMessages(chatRoom: ChatRoom, textView: TextView) {
+        val totalUnreadMessage = chatRoom.unread
+        when {
+            totalUnreadMessage in 1..99 -> {
+                textView.textContent = totalUnreadMessage.toString()
+                textView.setVisible(true)
+            }
+            totalUnreadMessage > 99 -> {
+                textView.textContent = getString(R.string.msg_more_than_ninety_nine_unread_messages)
+                textView.setVisible(true)
+            }
+            else -> textView.setVisible(false)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -170,10 +278,10 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
 
             for (chatRoom in newDataSet) {
                 LogUtil.d(TAG, "updateChatRooms>>" + chatRoom.toString())
-                if ((activity as MainActivity).username.equals(chatRoom.name)) {
+                if (mainActivity!!.username.equals(chatRoom.name)) {
 //                    LogUtil.d(TAG, "updateChatRooms has myVault @chatroom= " + chatRoom.toString())
-                    (activity as MainActivity).layoutMyVault.tag = chatRoom
-                    (activity as MainActivity).setupMyVault(chatRoom)
+                    layoutMyVault.tag = chatRoom
+                    setupMyVault(chatRoom)
                 } else {
                     dataSet.add(chatRoom)
                 }
