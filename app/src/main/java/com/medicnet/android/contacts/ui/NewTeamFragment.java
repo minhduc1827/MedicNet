c5acf687d584f57254569e6a7476be5274b6929d
Duc Nguyen <minhduc1827@gmail.com>
2018-06-10 15:32:17 +0700

    Adding newteam fragment

--- app/src/main/java/com/medicnet/android/contacts/ui/NewTeamFragment.java
+++ app/src/main/java/com/medicnet/android/contacts/ui/NewTeamFragment.java
@@ -9,10 +9,17 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.medicnet.android.R;
+import com.medicnet.android.contacts.adapter.UserSelectedAdapter;
 import com.medicnet.android.contacts.adapter.UsersAdapter;
+import com.medicnet.android.contacts.model.UserItem;
 import com.medicnet.android.main.ui.MainActivity;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author duc.nguyen
@@ -20,10 +27,16 @@ import com.medicnet.android.main.ui.MainActivity;
  */
 public class NewTeamFragment extends Fragment {
 
+    public static final String TAG = "NewTeamFragment";
+
     private RecyclerView recyclerViewUser;
+    private RecyclerView recyclerViewUserSelected;
     private MainActivity mainActivity;
     private UsersAdapter usersAdapter;
-    public static final String TAG = "NewTeamFragment";
+    private UserSelectedAdapter userSelectedAdapter;
+    private List<UserItem> listUser = new ArrayList<>();
+    private TextView txvUserSelectedEmpty;
+    private View mainView;
 
     public static NewTeamFragment newInstance() {
         NewTeamFragment fragment = new NewTeamFragment();
@@ -35,15 +48,60 @@ public class NewTeamFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_new_team, container, false);
-        recyclerViewUser = view.findViewById(R.id.recyclerViewUser);
+        mainView = inflater.inflate(R.layout.fragment_new_team, container, false);
         mainActivity = (MainActivity) getActivity();
+        txvUserSelectedEmpty = mainView.findViewById(R.id.txvUserSelectedEmpty);
+        setupRecyclerViewUser();
+        setupRecyclerViewUserSelected();
+        return mainView;
+    }
+
+    private void setupRecyclerViewUser() {
+        recyclerViewUser = mainView.findViewById(R.id.recyclerViewUser);
         LinearLayoutManager verticalLayoutmanager
                 = new LinearLayoutManager(mainActivity, LinearLayoutManager.VERTICAL, false);
         recyclerViewUser.setLayoutManager(verticalLayoutmanager);
         if (mainActivity.getUserList().size() > 0)
             usersAdapter = new UsersAdapter(mainActivity.getUserList());
+        usersAdapter.setOnUserListener(new UsersAdapter.OnUserListener() {
+            @Override
+            public void onUserSelected(boolean isCheck, UserItem user) {
+                updateUserSelectedList(isCheck, user);
+            }
+        });
         recyclerViewUser.setAdapter(usersAdapter);
-        return view;
+    }
+
+    private void updateUserSelectedList(boolean isCheck, UserItem user) {
+        if (isCheck)
+            listUser.add(user);
+        else {
+            Iterator<UserItem> iterator = listUser.iterator();
+            while (iterator.hasNext()) {
+                if (iterator.next().equals(user)) {
+                    iterator.remove();
+                }
+            }
+
+        }
+        if (listUser.size() > 0)
+            txvUserSelectedEmpty.setVisibility(View.GONE);
+        else
+            txvUserSelectedEmpty.setVisibility(View.GONE);
+        if (userSelectedAdapter == null) {
+            userSelectedAdapter = new UserSelectedAdapter(listUser);
+            recyclerViewUserSelected.setAdapter(userSelectedAdapter);
+        } else
+            userSelectedAdapter.notifyDataSetChanged();
+
+    }
+
+    private void setupRecyclerViewUserSelected() {
+        recyclerViewUserSelected = mainView.findViewById(R.id.recyclerViewUserSeleted);
+        LinearLayoutManager horizontalLayoutmanager
+                = new LinearLayoutManager(mainActivity, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewUserSelected.setLayoutManager(horizontalLayoutmanager);
+
+//        recyclerViewUserSelected.setAdapter(usersAdapter);
     }
 }
