fdd83cde02fbb45725be02a6fee00d781787adc7
duc.nguyen <duc.nguyen@filestring.com>
2018-06-12 15:48:51 +0700

    Adding butterknife gradle

--- app/src/main/java/com/medicnet/android/photo/ui/TakePhotoFragment.java
+++ app/src/main/java/com/medicnet/android/photo/ui/TakePhotoFragment.java
@@ -1,11 +1,16 @@
 package com.medicnet.android.photo.ui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 
 import com.medicnet.android.R;
@@ -14,40 +19,51 @@ import com.wonderkiln.camerakit.CameraKitEventCallback;
 import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTouch;
+
 /**
  * @author duc.nguyen
  * @since 6/12/2018
  */
 public class TakePhotoFragment extends Fragment {
 
-    private CameraView cameraView;
-    private View capture;
-    private ImageView facingButton;
+    public static final String TAG = "TakePhotoFragment";
+
+    @BindView(R.id.cameraView)
+    CameraView cameraView;
+    @BindView(R.id.facingButton)
+    ImageView facingButton;
+    @BindView(R.id.flashButton)
+    ImageView flashButton;
+    @BindView(R.id.blackCover)
+    View coverView;
+
+    @OnClick(R.id.imvCapture)
+    void onCaptureClicked() {
+        cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
+            @Override
+            public void callback(CameraKitImage cameraKitImage) {
+//                        ((MainActivity)getActivity()).handleTakePhoto(cameraKitImage.getBitmap());
+            }
+        });
+    }
+
+
     private int cameraMethod = CameraKit.Constants.METHOD_STANDARD;
     private boolean cropOutput = false;
-    public static final String TAG = "TakePhotoFragment";
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle
             savedInstanceState) {
         View view = inflater.inflate(R.layout.take_photo_fragment, container, false);
-        cameraView = view.findViewById(R.id.cameraView);
         cameraView.setMethod(cameraMethod);
         cameraView.setCropOutput(cropOutput);
-        capture = view.findViewById(R.id.imvCapture);
-        capture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
-                    @Override
-                    public void callback(CameraKitImage cameraKitImage) {
-//                        ((MainActivity)getActivity()).handleTakePhoto(cameraKitImage.getBitmap());
-                    }
-                });
-            }
-        });
-        facingButton = view.findViewById(R.id.facingButton);
+        ButterKnife.bind(this, view);
         return view;
     }
 
@@ -57,6 +73,128 @@ public class TakePhotoFragment extends Fragment {
         } else {
             facingButton.setImageResource(R.drawable.ic_facing_front);
         }
+    }
+
+
+    @OnTouch(R.id.facingButton)
+    boolean onTouchFacing(final View view, MotionEvent motionEvent) {
+        handleViewTouchFeedback(view, motionEvent);
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_UP: {
+                coverView.setAlpha(0);
+                coverView.setVisibility(View.VISIBLE);
+                coverView.animate()
+                        .alpha(1)
+                        .setStartDelay(0)
+                        .setDuration(300)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                super.onAnimationEnd(animation);
+                                if (cameraView.isFacingFront()) {
+                                    cameraView.setFacing(CameraKit.Constants.FACING_BACK);
+                                    changeViewImageResource((ImageView) view, R.drawable
+                                            .ic_facing_front);
+                                } else {
+                                    cameraView.setFacing(CameraKit.Constants.FACING_FRONT);
+                                    changeViewImageResource((ImageView) view, R.drawable
+                                            .ic_facing_back);
+                                }
+
+                                coverView.animate()
+                                        .alpha(0)
+                                        .setStartDelay(200)
+                                        .setDuration(300)
+                                        .setListener(new AnimatorListenerAdapter() {
+                                            @Override
+                                            public void onAnimationEnd(Animator animation) {
+                                                super.onAnimationEnd(animation);
+                                                coverView.setVisibility(View.GONE);
+                                            }
+                                        })
+                                        .start();
+                            }
+                        })
+                        .start();
+
+                break;
+            }
+        }
+        return true;
+    }
+
+    @OnTouch(R.id.flashButton)
+    boolean onTouchFlash(View view, MotionEvent motionEvent) {
+        handleViewTouchFeedback(view, motionEvent);
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_UP: {
+                if (cameraView.getFlash() == CameraKit.Constants.FLASH_AUTO) {
+                    cameraView.setFlash(CameraKit.Constants.FLASH_ON);
+                    changeViewImageResource((ImageView) view, R.drawable.ic_camera_flash_on);
+                } else if (cameraView.getFlash() == CameraKit.Constants.FLASH_ON) {
+                    cameraView.setFlash(CameraKit.Constants.FLASH_OFF);
+                    changeViewImageResource((ImageView) view, R.drawable.ic_camera_flash_off);
+                } else {
+                    cameraView.setFlash(CameraKit.Constants.FLASH_AUTO);
+                    changeViewImageResource((ImageView) view, R.drawable.ic_camera_flash_auto);
+                }
+
+                break;
+            }
+        }
+        return true;
+    }
+
+    void touchDownAnimation(View view) {
+        view.animate()
+                .scaleX(0.88f)
+                .scaleY(0.88f)
+                .setDuration(300)
+                .setInterpolator(new OvershootInterpolator())
+                .start();
+    }
+
+    void touchUpAnimation(View view) {
+        view.animate()
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(300)
+                .setInterpolator(new OvershootInterpolator())
+                .start();
+    }
+
+    boolean handleViewTouchFeedback(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                touchDownAnimation(view);
+                return true;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                touchUpAnimation(view);
+                return true;
+            }
+
+            default: {
+                return true;
+            }
+        }
+    }
+
+    void changeViewImageResource(final ImageView imageView, @DrawableRes final int resId) {
+        imageView.setRotation(0);
+        imageView.animate()
+                .rotationBy(360)
+                .setDuration(400)
+                .setInterpolator(new OvershootInterpolator())
+                .start();
+
+        imageView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setImageResource(resId);
+            }
+        }, 120);
     }
 
     @Override
